cb175a9d6cfb0084239314001c95b3291994cac2
najdawi11 <nathernajdawi@gmail.com>
2023-05-12 08:02:03 +0300

    files

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,2 @@
+weather_api_key = "6eb99c31322c10fb56f41dfe5ad5631e"
+geoapify_key="4850bde458949e78672d029fdebb822"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "6eb99c31322c10fb56f41dfe5ad5631e"
+
+# Geoapify API Key
+geoapify_key = "4850bde458949e78672d029fdebb822"
